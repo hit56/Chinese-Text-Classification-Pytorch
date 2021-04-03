dc54c1d534f5089ccf266e56423725acb2084dd7
zhenghong <zhenghong@hit56.com>
2021-04-03 20:32:44 +0800

    增加run.sh

---
+++ run.sh
@@ -0,0 +1,22 @@
+set -x
+# 训练并测试：
+# TextCNN
+python run.py --model TextCNN
+
+# TextRNN
+python run.py --model TextRNN
+
+# TextRNN_Att
+python run.py --model TextRNN_Att
+
+# TextRCNN
+python run.py --model TextRCNN
+
+# FastText, embedding层是随机初始化的
+python run.py --model FastText --embedding random 
+
+# DPCNN
+python run.py --model DPCNN
+
+# Transformer
+python run.py --model Transformer
